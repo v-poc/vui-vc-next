be6304060d9470585f41bd7c3d0d992fa487c542
Niko <bluepspower@163.com>
2020-07-22 00:38:32 +0800

    chore: bump deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,11 @@
-# 0.0.3
+# 0.0.3 (2020-07-19)
 
 Added Tag demo.
 
-# 0.0.2
+# 0.0.2 (2020-07-19)
 
 Added Skeleton demo.
 
-# 0.0.1
+# 0.0.1 (2020-07-18)
 
 Initial commit.
--- src/views/TagDemo.vue
+++ src/views/TagDemo.vue
@@ -1,30 +1,28 @@
 <template>
-  <div id="app">
-    <router-link to="/" class="m-back">&lt; Home</router-link>
-    <div class="m-example">
-      <p>Tag Shape - Circle</p>
-      <v-tag type="fill" size="tiny" shape="circle" fill-color="#FC5353">100</v-tag>
-      <v-tag type="fill" size="small" shape="circle" fill-color="#36C" font-color="#FFF">promotion</v-tag>
-      <v-tag type="ghost" size="small" shape="circle">10%</v-tag>
-      <v-tag
-        type="fill"
-        size="small"
-        shape="circle"
-        sharp="bottom-right"
-        fill-color="#36C"
-        font-color="#FFF"
-      >half price</v-tag>
-    </div>
-    <div class="m-example">
-      <p>Tag Shape - Special</p>
-      <v-tag type="fill" shape="coupon" fill-color="#36C" font-color="#FFF">Coupon Ticket</v-tag>
-      <v-tag type="fill" shape="quarter" fill-color="#FC9191" font-color="#FFF">&times;</v-tag>
-      <v-tag type="fill" shape="bubble" font-color="#FFF">88</v-tag>
-    </div>
-    <div class="m-example">
-      <p>Tag Shape - No fill color</p>
-      <v-tag type="fill" shape="square" fill-color="rgba(0, 0, 0, 0)">&pound;100</v-tag>
-    </div>
+  <router-link to="/" class="m-back">&lt; Home</router-link>
+  <div class="m-example">
+    <p>Tag Shape - Circle</p>
+    <v-tag type="fill" size="tiny" shape="circle" fill-color="#FC5353">100</v-tag>
+    <v-tag type="fill" size="small" shape="circle" fill-color="#36C" font-color="#FFF">promotion</v-tag>
+    <v-tag type="ghost" size="small" shape="circle">10%</v-tag>
+    <v-tag
+      type="fill"
+      size="small"
+      shape="circle"
+      sharp="bottom-right"
+      fill-color="#36C"
+      font-color="#FFF"
+    >half price</v-tag>
+  </div>
+  <div class="m-example">
+    <p>Tag Shape - Special</p>
+    <v-tag type="fill" shape="coupon" fill-color="#36C" font-color="#FFF">Coupon Ticket</v-tag>
+    <v-tag type="fill" shape="quarter" fill-color="#FC9191" font-color="#FFF">&times;</v-tag>
+    <v-tag type="fill" shape="bubble" font-color="#FFF">88</v-tag>
+  </div>
+  <div class="m-example">
+    <p>Tag Shape - No fill color</p>
+    <v-tag type="fill" shape="square" fill-color="rgba(0, 0, 0, 0)">&pound;100</v-tag>
   </div>
 </template>
 
