f3208997cefae9b326a0599a2be6e779734beb85
Niko <bluepspower@163.com>
2020-07-23 01:58:32 +0800

    chore: bump deps

---
+++ src/components/transition/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <transition :name="name">
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'v-transition',
+
+  // functional: true,
+
+  props: {
+    name: {
+      type: String,
+      default: 'm-fade',
+      required: true
+    }
+  }
+})
+</script>
