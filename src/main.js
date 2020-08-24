2bb96b40a487855025d637655a816643082d4c3a
Niko <bluepspower@163.com>
2020-08-25 01:43:46 +0800

    chore: update styles

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers/index'
 
-import './assets/styles/common.scss'
-
 createApp(App)
   .use(router)
   .mount('#app')
