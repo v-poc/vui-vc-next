354b34befa818e1080cc38b2bd5aaba99fab32c7
Niko <bluepspower@163.com>
2020-09-06 19:45:17 +0800

    chore: add fastclick for touch device

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers/index'
-
+import FastClickDefault, { FastClick } from 'fastclick'
+// import common reset css
 import './assets/styles/vui-reset.scss'
+
+// handle fastclick
+if ('ontouchstart' in window) {
+  const _attach = FastClickDefault.attach || FastClick.attach
+  _attach && _attach(document.body)
+}
 
 createApp(App)
   .use(router)
