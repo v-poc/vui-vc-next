897b06bf044af9fbfd76bcd005da1c8a296988e7
Niko <bluepspower@163.com>
2020-09-07 02:26:51 +0800

    chore: update fastclick reference

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers/index'
-import FastClickDefault, { FastClick } from 'fastclick'
 // import common reset css
 import './assets/styles/vui-reset.scss'
 
 // handle fastclick
 if ('ontouchstart' in window) {
-  const _attach = FastClickDefault.attach || FastClick.attach
+  const _attach = window.FastClick && window.FastClick.attach
   _attach && _attach(document.body)
 }
 
