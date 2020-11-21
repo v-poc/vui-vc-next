cbfc681d29b5abb76074697102c7099c66f3a0d5
Niko <bluepspower@163.com>
2020-11-21 14:36:45 +0800

    docs: update doc config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,29 @@
 module.exports = {
+  lang: 'en-US',
   title: 'VUI docs',
-  description: 'The docs of VUI'
+  description: 'The docs of VUI.next',
+
+  themeConfig: {
+    repo: 'bluepower/vui-vc-next',
+    docsDir: 'docs',
+
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+
+    nav: [
+      { text: 'API Reference', link: '/' },
+      {
+        text: 'Changelog',
+        link: 'https://github.com/bluepower/vui-vc-next/blob/dev/CHANGELOG.md'
+      }
+    ],
+
+    sidebar: {
+      '/': getConfigSidebar()
+    }
+  }  
+}
+
+function getConfigSidebar() {
+  return [{ text: 'API Reference', link: '/' }]
 }
--- docs/index.md
+++ docs/index.md
@@ -1,7 +1,3 @@
----
-sidebar: auto
----
-
 # VUI docs
 
 ## Reference list
