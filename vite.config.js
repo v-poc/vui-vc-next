1291d6cedea67216b85c9f2668153e2b687fe4ad
Niko <bluepspower@163.com>
2020-12-25 02:44:16 +0800

    release: v0.8.4

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  base: '/vui-vc-next',
+  base: '/vui-vc-next', // for production
+  outDir: 'dist/vui-vc-next', // npm run serve-build => http://localhost:3000/vui-vc-next
   port: 8080
 }
