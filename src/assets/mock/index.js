e5154651ab7617260e4e798feb847a373d218da7
Niko <bluepspower@163.com>
2020-12-29 01:57:09 +0800

    chore: update mock imgUrl

--- src/assets/mock/index.js
+++ src/assets/mock/index.js
@@ -3,78 +3,78 @@ export const PHOTOS_DATA = [
     id: '3646653877',
     name: 'accusamus beatae',
     desc: 'accusamus beatae ad facilis cum similique qui sunt',
-    imgUrl: '//via.placeholder.com/150/92c952'
+    imgUrl: 'http://via.placeholder.com/150/92c952'
   },
   {
     id: '1789676645',
     name: 'reprehenderit est',
     desc: 'reprehenderit est deserunt velit ipsam',
-    imgUrl: '//via.placeholder.com/150/771796'
+    imgUrl: 'http://via.placeholder.com/150/771796'
   },
   {
     id: '3649034125',
     name: 'officia porro',
     desc: 'officia porro iure quia iusto qui ipsa ut modi',
-    imgUrl: '//via.placeholder.com/150/24f355'
+    imgUrl: 'http://via.placeholder.com/150/24f355'
   },
   {
     id: '2977781451',
     name: 'culpa odio',
     desc: 'culpa odio esse rerum omnis laboriosam voluptate repudiandae',
-    imgUrl: '//via.placeholder.com/150/d32776'
+    imgUrl: 'http://via.placeholder.com/150/d32776'
   },
   {
     id: '2691498608',
     name: 'natus nisi',
     desc: 'natus nisi omnis corporis facere molestiae rerum in',
-    imgUrl: '//via.placeholder.com/150/f66b97'
+    imgUrl: 'http://via.placeholder.com/150/f66b97'
   },
   {
     id: '3703029362',
     name: 'accusamus ea',
     desc: 'accusamus ea aliquid et amet sequi nemo',
-    imgUrl: '//via.placeholder.com/150/56a8c2'
+    imgUrl: 'http://via.placeholder.com/150/56a8c2'
   },
   {
     id: '3606666961',
     name: 'officia delectus',
     desc: 'officia delectus consequatur vero aut veniam explicabo molestias',
-    imgUrl: '//via.placeholder.com/150/b0f7cc'
+    imgUrl: 'http://via.placeholder.com/150/b0f7cc'
   },
   {
     id: '3604823742',
     name: 'aut porro',
     desc: 'aut porro officiis laborum odit ea laudantium corporis',
-    imgUrl: '//via.placeholder.com/150/54176f'
+    imgUrl: 'http://via.placeholder.com/150/54176f'
   },
   {
     id: '1746389460',
     name: 'qui eius',
     desc: 'qui eius qui autem sed',
-    imgUrl: '//via.placeholder.com/150/51aa97'
+    imgUrl: 'http://via.placeholder.com/150/51aa97'
   },
   {
     id: '1746020155',
     name: 'beatae et',
     desc: 'beatae et provident et ut vel',
-    imgUrl: '//via.placeholder.com/150/810b14'
+    imgUrl: 'http://via.placeholder.com/150/810b14'
   },
   {
     id: '2949232733',
     name: 'nihil at',
     desc: 'nihil at amet non hic quia qui',
-    imgUrl: '//via.placeholder.com/150/1ee8a4'
+    imgUrl: 'http://via.placeholder.com/150/1ee8a4'
   },
   {
     id: '3691421503',
     name: 'mollitia soluta',
     desc: 'mollitia soluta ut rerum eos aliquam consequatur perspiciatis maiores',
-    imgUrl: '//via.placeholder.com/150/66b7d2'
+    imgUrl: 'http://via.placeholder.com/150/66b7d2'
   },
   {
     id: '2072789141',
     name: 'repudiandae iusto',
     desc: 'repudiandae iusto deleniti rerum',
-    imgUrl: '//via.placeholder.com/150/197d29'
+    imgUrl: 'http://via.placeholder.com/150/197d29'
   }
 ]
