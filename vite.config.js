5a15d3823bfc8fc50e9639cdde2e8784b7abc06f
Niko <bluepspower@163.com>
2021-01-02 23:56:31 +0800

    build: adapt vite.config.js for vite2

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
   build: {
     base: '/vui-vc-next/', // for production
     outDir: 'dist/vui-vc-next' // npm run serve-build => http://localhost:3000/vui-vc-next/
+  },
+  server: {
+    port: 8080
   }
-  // port: 8080
 })
