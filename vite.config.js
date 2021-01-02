a76eec76b560795a6b15489d6c96574461735296
Niko <bluepspower@163.com>
2021-01-02 20:48:18 +0800

    build: adapt vite.config.js for vite2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import vuePlugin from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [ vuePlugin() ], // for plugin
-  base: '/vui-vc-next', // for production
-  outDir: 'dist/vui-vc-next' // npm run serve-build => http://localhost:3000/vui-vc-next
+  build: {
+    base: '/vui-vc-next/', // for production
+    outDir: 'dist/vui-vc-next' // npm run serve-build => http://localhost:3000/vui-vc-next/
+  }
   // port: 8080
 })
